aa31c928a9be60a9f329de102621277904868789
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-07-04 09:11:56 -0400

    Parallelize downloading

--- install.sh
+++ install.sh
@@ -11,9 +11,10 @@ cd $tempdir
 
 _download version
 if [ ! -f /usr/lib/libscone.so.`cat version` ]; then
-	_download scone.c
-	_download scone.h
-	_download Makefile
+	_download scone.c &
+	_download scone.h &
+	_download Makefile &
+	wait
 	sudo make install
 fi
 
